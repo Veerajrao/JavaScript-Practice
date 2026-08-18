5f32c925017061e5c3c9cf29b27b31472291a920
Veeraj <veerajrao05@gmail.com>
2026-08-18 22:14:36 +0530

    added eventListener using foreach

--- JavaScript_Projects/ToDoList/todolists.js
+++ JavaScript_Projects/ToDoList/todolists.js
@@ -6,11 +6,23 @@ function renderToDoList(){
         const todoobj = todolist[i];
         const { name, duedate } = todoobj;
         const html =
-         `<div>${name}</div><div>${duedate}</div><button class="dlt-btn" onclick = "removeToDo(${i})">Delete</button>`;
+         `<div>${name}</div><div>${duedate}</div><button class="dlt-btn js-dlt-btn" >Delete</button>`;
         todolits += html;
     }
     document.querySelector('.js-todo-lists').innerHTML = todolits;
+
+    document.querySelectorAll('.js-dlt-btn').forEach( (deleteElement, index) =>{
+        deleteElement.addEventListener('click', () => {
+            removeToDo(index, 1);
+        })
+    })
 }
+
+//adding event lsitener
+document.querySelector('.js-add-btn').addEventListener('click', () =>{
+    gettodo();
+})
+
 function gettodo(){
     const inele = document.querySelector('.js-input-todo');
     const ele = inele.value;
